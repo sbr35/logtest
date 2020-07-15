737bf214aee5ac736d8d5c21ad0a1b75abfaa19c
sbr35 <shohidulbari18@gmail.com>
2020-07-15 17:51:01 +0600

    hello world

---
+++ handlers/handler.go
@@ -0,0 +1,7 @@
+package handlers
+
+import "fmt"
+
+func MyFunc() {
+	fmt.Println("This is from hello handler")
+}
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/sbr35/logtest/handlers"
+)
+
+func main() {
+	fmt.Println("Hello world")
+	handlers.MyFunc()
+}
